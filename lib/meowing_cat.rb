ba2a106823a67e527689406249898f677ff32746
Scarlett Perry <si.perry15@gmail.com>
2018-07-18 17:44:25 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,18 @@
 ## code your solution here. 
+class Cat 
+  attr_accessor :name
+  
+  #def initialize(name)
+  #  @name = name
+  #end 
+  
+  def meow
+    puts "meow!"
+  end 
+end 
+
+maru = Cat.new
+maru.meow
+maru.name = "Maru"
+puts maru.name #getter does not automatically puts the return value
+
